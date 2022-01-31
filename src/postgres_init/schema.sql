848ca9c0517a5fdef4d8cb4421d2f45579c58018
cosa65 <martin@biomage.net>
2022-01-31 17:48:27 -0300

    Add switch to table line

--- src/postgres_init/schema.sql
+++ src/postgres_init/schema.sql
@@ -1,10 +1,12 @@
 -- On creation we are connected to development
 CREATE DATABASE development;
 
+\c development;
+
 REVOKE ALL ON DATABASE development FROM public;
 
 -- The role used by the api
-CREATE ROLE read_write_role;
+CREATE ROLE read_write_role WITH LOGIN PASSWORD 'password';
 
 GRANT CONNECT ON DATABASE development TO read_write_role;
 
@@ -15,3 +17,16 @@ ALTER DEFAULT PRIVILEGES
   FOR ROLE read_write_role
   IN SCHEMA public
   GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO read_write_role;
+
+-- Tables
+CREATE TABLE "experiment" (
+  "id" UUID,
+  "name" VARCHAR,
+  "description" VARCHAR,
+  "processing_config" JSONB,
+  "created_at" TIMESTAMP,
+  "updated_at" TIMESTAMP,
+  "notify_by_email" BOOLEAN,
+  PRIMARY KEY ("id")
+);
+
