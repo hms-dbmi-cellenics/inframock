caf722786062d0fde2608a6e1d7a77ea75e5df8c
Pol Alvarez <pol.avms@gmail.com>
2022-03-07 10:33:55 +0000

    added dev_role to postgres

--- src/postgres_init/schema.sql
+++ src/postgres_init/schema.sql
@@ -4,24 +4,13 @@ CREATE DATABASE aurora_db;
 
 \c aurora_db;
 
--- The role used by the api
+-- The role used by the api & develop
 CREATE ROLE api_role WITH LOGIN PASSWORD 'password';
+CREATE ROLE dev_role WITH LOGIN PASSWORD 'password';
 
 GRANT USAGE ON SCHEMA public TO api_role;
+GRANT USAGE ON SCHEMA public TO dev_role;
 
 -- Gives these privileges to api_role on any table that we create in public
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO api_role;
-
--- This isn't where we are going to create the tables and run the migrations
--- It should be done be in the python code once we have defined a mechanism for running migrations
-CREATE TABLE "experiment" (
-  "id" UUID,
-  "name" VARCHAR,
-  "description" VARCHAR,
-  "processing_config" JSONB,
-  "created_at" TIMESTAMP,
-  "updated_at" TIMESTAMP,
-  "notify_by_email" BOOLEAN,
-  PRIMARY KEY ("id")
-);
-
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO dev_role;
